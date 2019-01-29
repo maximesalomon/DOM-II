40c517a11353bf360d536b77e7a7be2c81da8862
Maxime Salomon <maxime@croissant.io>
2019-01-29 12:27:35 +0100

    Mouseover Event Listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const navbar = document.getElementsByClassName('nav-link');
+console.log(navbar);
+navbar[3].addEventListener("mouseover", function(colorChange) {
+    colorChange.target.style.color = "yellow";
+});
