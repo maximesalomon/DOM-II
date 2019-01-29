bc01b2dba4ab449ffa1928099233d57443376d71
Maxime Salomon <maxime@croissant.io>
2019-01-29 15:09:11 +0100

    Prevent Default + MVP

--- js/index.js
+++ js/index.js
@@ -1,66 +1,84 @@
 // Your code goes here
 
-// // Mouseover
-// const navbar = document.getElementsByClassName('nav-link');
-// console.log(navbar);
-// navbar[3].addEventListener("mouseover", function(colorChange) {
-//     colorChange.target.style.color = "yellow";
-// });
+// Mouseover
+const navbar = document.getElementsByClassName('nav-link');
+navbar[3].addEventListener("mouseover", function(colorChange) {
+    colorChange.target.style.color = "yellow";
+});
 
-// // Mousedown
-// navbar[3].addEventListener("mousedown", function(colorChange) {
-//     colorChange.target.style.color = "red";
-// });
-
-
-// // Keydown
-// document.addEventListener('keydown', function() {
-//     alert(`You have pressed a key`);
-// }, false);
+// Mousedown
+navbar[3].addEventListener("mousedown", function(colorChange) {
+    colorChange.target.style.color = "red";
+});
 
 
-// // Wheel
-// document.addEventListener('wheel', function() {
-//     alert(`You have wheeled`);
-// }, false);
+// Keydown
+document.addEventListener('keydown', function() {
+    console.log(`You have pressed a key`);
+}, false);
 
 
-// // Drag & Drop
-// document.addEventListener('dragstart', function() {
-//     alert(`You have started to drag`);
-// }, false);
+// Wheel
+document.addEventListener('wheel', function() {
+    console.log(`You have wheeled`);
+}, false);
 
-// // Load
-// window.addEventListener("load", function(event) {
-//     alert("The page has been loaded");
-//   });
+
+// Drag & Drop
+document.addEventListener('dragstart', function() {
+    console.log(`You have started to drag`);
+}, false);
+
+// Load
+window.addEventListener("load", function(event) {
+    console.log("The page has been loaded");
+  });
 
 // // Focus
 // window.addEventListener('focus', function(event) {
-//     alert("The page has focused");
+//     console.log("The page has focused");
 // });
 
-// // Resize
-// window.addEventListener('resize', function(event) {
-//     alert("The page has resized");
-// });
-
-// // Scroll
-// window.addEventListener('scroll', function(event) {
-//     alert("The page has scroll");
-// });
-
-// // Select
-// var input = document.getElementsByTagName('input');
-
-// input[0].addEventListener('select', function(event) {
-//     alert("The input field has been selected");
-// });
-
-// dbclick
-
-var busImage = document.getElementsByTagName('header');
-
-busImage.addEventListener('dbclick', function(event) {
-    alert("The page has been double clicked");
+// Resize
+window.addEventListener('resize', function(event) {
+    console.log("The page has resized");
 });
+
+// Scroll
+window.addEventListener('scroll', function(event) {
+    console.log("The page has scroll");
+});
+
+// Select
+var input = document.getElementsByTagName('input');
+
+input[0].addEventListener('select', function(event) {
+    console.log("The input field has been selected");
+});
+
+// Dblclick
+
+window.addEventListener('dblclick', function(event) {
+    console.log("The page has been double clicked");
+});
+
+
+// Stop Propagation
+
+const welcome = document.getElementsByTagName('h2');
+welcome[0].addEventListener('click', function(event){
+    console.log('welcome clicked propagation');
+    event.stopPropagation();
+});
+
+const welcomeSection = document.getElementsByClassName('intro');
+welcomeSection[0].addEventListener('click', function(event){
+    console.log('welcome clicked STOP propagation');
+});
+
+
+// Prevent Default
+navbar[0].addEventListener('click', function(event) {
+    event.preventDefault();
+});
+
